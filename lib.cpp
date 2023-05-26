bcd75e7af11faf806a137dbb72ed7371d7015f03
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-05-26 22:37:45 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include "lib.h"
+#include <iostream>
+using namespace std;
+void inizializza(int vettore[], int n){
+    for (int i = 0; i < n; i++) {
+        vettore[i] = 0;
+        
+    }
+}
 
+int compara(int vettore[], int n) {
+    int temp = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> vettore[i];
+        if (vettore[i] > temp) {
+            temp = vettore[i];
+        }
+    }
+    return temp;
+}
