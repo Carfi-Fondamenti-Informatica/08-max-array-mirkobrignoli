ff701eb1e4f55e3a3e663358e5c5359b5bfccb98
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-05-26 22:38:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
+#include "lib.h"
 #include <iostream>
 
-int main(){
-  return 0;
+using namespace std;
+
+int main()
+{
+    int n,risultato;
+    cout << "quanti numeri vuoi inserire?"<< endl;
+    cin >> n;
+    int vettore[n];
+    
+    inizializza(vettore,n);
+    risultato = compara(vettore, n);
+    
+    cout << risultato << endl;
+
+    return 0;
 }
